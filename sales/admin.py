7a354000f3890116bd8bef0f7ab017a21e0879af
Given <moretigiven@gmail.com>
2024-03-21 14:23:57 +0200

    Add sales,Csv and position

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Sale,CSV,Position
 # Register your models here.
+admin.site.register(Sale)
+admin.site.register(CSV)
+admin.site.register(Position)
