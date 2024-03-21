ea63cebdb5514f7c7f2657c4c35263bfd4c01302
Given <moretigiven@gmail.com>
2024-03-21 13:35:23 +0200

    Add Customer Models

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to="customers",default="default.jpg")
+
+    def __str__(self):
+        return self.name
